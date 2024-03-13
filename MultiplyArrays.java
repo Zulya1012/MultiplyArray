d21087c35a6348d65742d1683f2e512140fb8850
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-03-13 16:57:21 +0400

    Update MultiplyArrays.java

--- MultiplyArrays.java
+++ MultiplyArrays.java
@@ -1,26 +1,35 @@
 public class MultiplyArrays {
 
-    public static void main(String[] args) {
-        int[] array1 = {1, 3, -5, 4};
-        int[] array2 = {1, 4, -5, -2};
+  public static void main(String[] args) {
+    int[] array1 = {
+      1,
+      3,
+      -5,
+      4
+    };
+    int[] array2 = {
+      1,
+      4,
+      -5,
+      -2
+    };
 
-        
-        int[] result = multiplyArrays(array1, array2);
+    int[] result = multiplyArrays(array1, array2);
 
-        System.out.println("Result: " + java.util.Arrays.toString(result));
+    System.out.println("Result: " + java.util.Arrays.toString(result));
+  }
+
+  public static int[] multiplyArrays(int[] arr1, int[] arr2) {
+    if (arr1.length != arr2.length) {
+      throw new IllegalArgumentException("Arrays must have the same length");
     }
 
-    public static int[] multiplyArrays(int[] arr1, int[] arr2) {
-        if (arr1.length != arr2.length) {
-            throw new IllegalArgumentException("Arrays must have the same length");
-        }
+    int[] result = new int[arr1.length];
 
-        int[] result = new int[arr1.length];
-
-        for (int i = 0; i < arr1.length; i++) {
-            result[i] = arr1[i] * arr2[i];
-        }
-
-        return result;
+    for (int i = 0; i < arr1.length; i++) {
+      result[i] = arr1[i] * arr2[i];
     }
+
+    return result;
+  }
 }
